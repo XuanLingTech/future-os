08e102ab7ea3cea4de2525a8522865dbe757c556
陈小康 <4762210@qq.com>
2021-08-25 12:04:31 +0800

    取消指定打包目录

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'os'
-  },
   server: {
     host: '0.0.0.0',
     port: 8888,
